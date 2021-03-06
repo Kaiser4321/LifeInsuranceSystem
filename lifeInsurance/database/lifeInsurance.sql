7d5464c8da502b614307479de412683ed266087f
Kaiser Dianalan <72648217+Kaiser4321@users.noreply.github.com>
2021-03-06 16:30:26 +0800

    Project GitHub Link

---
+++ lifeInsurance/database/lifeInsurance.sql
@@ -0,0 +1,168 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 25, 2020 at 02:17 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.5.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `lims`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agent`
+--
+
+CREATE TABLE IF NOT EXISTS `agent` (
+  `agent_id` varchar(50) NOT NULL,
+  `agent_password` varchar(30) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `branch` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `agent`
+--
+
+INSERT INTO `agent` (`agent_id`, `agent_password`, `name`, `branch`, `phone`) VALUES
+('ABDUL', 'admin', 'Abdul Salsalani', 'Taguig', '091222nog-tunog');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE IF NOT EXISTS `client` (
+  `client_id` varchar(200) NOT NULL,
+  `client_password` varchar(30) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `sex` varchar(20) NOT NULL,
+  `birth_date` varchar(20) NOT NULL,
+  `marital_status` varchar(20) NOT NULL,
+  `nid` varchar(50) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `policy_id` varchar(50) NOT NULL,
+  `agent_id` varchar(20) NOT NULL,
+  `image` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `client`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nominee`
+--
+
+CREATE TABLE IF NOT EXISTS `nominee` (
+  `nominee_id` varchar(200) NOT NULL,
+  `client_id` varchar(100) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `sex` varchar(20) NOT NULL,
+  `birth_date` varchar(50) NOT NULL,
+  `nid` varchar(50) NOT NULL,
+  `relationship` varchar(20) NOT NULL,
+  `priority` varchar(20) NOT NULL,
+  `phone` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `nominee`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE IF NOT EXISTS `payment` (
+  `recipt_no` varchar(20) NOT NULL,
+  `client_id` varchar(50) NOT NULL,
+  `month` varchar(50) NOT NULL,
+  `amount` varchar(50) NOT NULL,
+  `due` varchar(50) NOT NULL,
+  `fine` varchar(50) NOT NULL,
+  `agent_id` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `payment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `policy`
+--
+
+CREATE TABLE IF NOT EXISTS `policy` (
+  `policy_id` varchar(50) NOT NULL,
+  `term` varchar(50) NOT NULL,
+  `health_status` varchar(50) NOT NULL,
+  `system` varchar(50) NOT NULL,
+  `payment_method` varchar(50) NOT NULL,
+  `coverage` varchar(50) NOT NULL,
+  `age_limit` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `policy`
+--
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `agent`
+--
+ALTER TABLE `agent`
+ ADD PRIMARY KEY (`agent_id`), ADD UNIQUE KEY `agent_id` (`agent_id`);
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+ ADD PRIMARY KEY (`client_id`), ADD UNIQUE KEY `client_id` (`client_id`);
+
+--
+-- Indexes for table `nominee`
+--
+ALTER TABLE `nominee`
+ ADD PRIMARY KEY (`nominee_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+ ADD PRIMARY KEY (`recipt_no`), ADD UNIQUE KEY `recipt_no` (`recipt_no`);
+
+--
+-- Indexes for table `policy`
+--
+ALTER TABLE `policy`
+ ADD PRIMARY KEY (`policy_id`), ADD UNIQUE KEY `policy_id` (`policy_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
